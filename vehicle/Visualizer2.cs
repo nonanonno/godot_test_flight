396e864d7475e392a33e119ff9c848ee7b7c6cd4
nonanonno <as.nonanonno@gmail.com>
2023-10-28 15:01:51 +0900

    update

--- vehicle/Visualizer2.cs
+++ vehicle/Visualizer2.cs
@@ -56,6 +56,7 @@ public partial class Visualizer2 : Node2D
 
     public override void _Draw()
     {
-        Drawer.Draw(this, Radius, Color, Width);
+        DrawArc(Vector2.Zero, Radius, 0, 360, 60, Color, Width);
+        DrawLine(Vector2.Zero, new Vector2(Radius, 0), Color, Width);
     }
 }

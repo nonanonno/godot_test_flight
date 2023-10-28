057e455bccd9ac4a00f31704b4041e741c145a16
nonanonno <as.nonanonno@gmail.com>
2023-10-28 13:51:30 +0900

    add vehicle

--- Node2D.cs
+++ Node2D.cs
@@ -6,8 +6,6 @@ public partial class Node2D : Godot.Node2D
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-		GD.Print("Hello");
-		GD.Print("Hello");
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
--- scripts/Hello.cs
+++
@@ -1,8 +0,0 @@
-class HelloWorld
-{
-    public void foo()
-    {
-        GD.Print("Hello");
-        GD.Print("Hello");
-    }
-}
